f75033bf8d4da5e72d3943a7324cd6b937ad9ec3
iniperusahaanbaru <158676619+iniperusahaanbaru@users.noreply.github.com>
2024-05-12 16:55:49 +0700

    Update trial.py

--- trial.py
+++ trial.py
@@ -4,24 +4,28 @@ from google.oauth2.service_account import Credentials
 
 # Authentication with Google Sheets
 def authenticate_gsheets():
-    credentials_dict = {
-        'type': st.secrets["TYPE"],
-        'project_id': st.secrets["PROJECT_ID"],
-        'private_key_id': st.secrets["PRIVATE_KEY_ID"],
-        'private_key': st.secrets["PRIVATE_KEY"].replace('\\n', '\n'),
-        'client_email': st.secrets["CLIENT_EMAIL"],
-        'client_id': st.secrets["CLIENT_ID"],
-        'auth_uri': st.secrets["AUTH_URI"],
-        'token_uri': st.secrets["TOKEN_URI"],
-        'auth_provider_x509_cert_url': st.secrets["AUTH_PROVIDER_X509_CERT_URL"],
-        'client_x509_cert_url': st.secrets["CLIENT_X509_CERT_URL"],
-    }
-    # Adding the scope for accessing Google Sheets
-    scopes = ['https://www.googleapis.com/auth/spreadsheets']
-    creds = Credentials.from_service_account_info(credentials_dict, scopes=scopes)
-    client = gspread.authorize(creds)
-    sheet = client.open("Portfolio Requests").sheet1
-    return sheet
+    try:
+        credentials_dict = {
+            'type': st.secrets["TYPE"],
+            'project_id': st.secrets["PROJECT_ID"],
+            'private_key_id': st.secrets["PRIVATE_KEY_ID"],
+            'private_key': st.secrets["PRIVATE_KEY"].replace('\\n', '\n'),
+            'client_email': st.secrets["CLIENT_EMAIL"],
+            'client_id': st.secrets["CLIENT_ID"],
+            'auth_uri': st.secrets["AUTH_URI"],
+            'token_uri': st.secrets["TOKEN_URI"],
+            'auth_provider_x509_cert_url': st.secrets["AUTH_PROVIDER_X509_CERT_URL"],
+            'client_x509_cert_url': st.secrets["CLIENT_X509_CERT_URL"],
+        }
+        # Adding the scope for accessing Google Sheets
+        scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        creds = Credentials.from_service_account_info(credentials_dict, scopes=scopes)
+        client = gspread.authorize(creds)
+        sheet = client.open("Portfolio Requests").sheet1
+        return sheet
+    except Exception as e:
+        st.error(f"Failed to authenticate with Google Sheets: {str(e)}")
+        return
 
 def main():
     st.title("Welcome to Your Portfolio")
